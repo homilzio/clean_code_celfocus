2f7b926e06ebd6fdfd2307ad01498dfafb9b8de0
trovoada <homilzio.santos@celfocus.com>
2020-06-19 18:44:09 +0100

    [HTS] - added exceptions and error handler class

--- clean_code_celfocus/src/main/java/RentCarApp.java
+++ clean_code_celfocus/src/main/java/RentCarApp.java
@@ -1,7 +1,15 @@
+import account.Account;
+import account.AccountImpl;
+import error.RentCarAppError;
+import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import person.Client;
+import person.Person;
 import person.Salesman;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class RentCarApp {
     private static final Logger LOGGER = LoggerFactory.getLogger(RentCarApp.class);
@@ -12,6 +20,30 @@ public class RentCarApp {
         //TODO add relevant type
         createNewUser();
         Salesman newSalesman = createNewSalesman();
+
+        // TODO give proper names and validate
+           /* Person p = new Client();
+            Client c = new Client();
+            */
+
+        // TODO install Lombok plugin
+
+        try {
+            Client alberto = createNewClientErrorTest("Alberto", 18, true, Arrays.asList(new AccountImpl(), new AccountImpl()));
+        } catch (Exception e) {
+            //TODO Logg exception properly
+            e.printStackTrace();
+        }
+
+    }
+
+    private <T> Client createNewClientErrorTest(String alberto,  int age, boolean canBuyACar, List<T> accountList) throws Exception {
+        // TODO implement method body -> validate if name is acceptable and check the minimum age to buy a car
+        throw RentCarAppError.METHOD_NOT_IMPLEMENTED;
+    }
+
+    private static Client createNewClientErrorTest(String alberto, int age, boolean canBuyACar) {
+        return null;
     }
 
     public static void printWelcomeMessage(){
